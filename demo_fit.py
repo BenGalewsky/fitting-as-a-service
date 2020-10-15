f44f83de7f87c028a9889ad48fcc8d8acb62aed6
Ben Galewsky <ben@peartreestudio.net>
2020-10-15 13:40:56 -0500

    Split workspace setup from fit

--- demo_fit.py
+++ demo_fit.py
@@ -8,9 +8,19 @@ pyhf_endpoint = 'ae7f555f-07ec-4261-b5f6-d0930ce545a5'
 fxc = FuncXClient()
 
 
-def hello_pyhf(data, doc):
+def prepare_workspace(data):
     import pyhf
     w = pyhf.Workspace(data)
+    return w
+
+prepare_func = fxc.register_function(prepare_workspace)
+
+
+def infer_hypotest(w, doc):
+    import pyhf
+    import time
+
+    tick = time.time()
     m = w.model(
         patches=[doc],
         modifier_settings={
@@ -20,16 +30,33 @@ def hello_pyhf(data, doc):
     )
     d = w.data(m)
     return {
-        'CLs_obs': float(pyhf.infer.hypotest(1.0, d, m, qtilde=True))
+        'CLs_obs': float(pyhf.infer.hypotest(1.0, d, m, qtilde=True)),
+        'Fit-Time': time.time() - tick
     }
 
 
-f = fxc.register_function(hello_pyhf)
+f = fxc.register_function(infer_hypotest)
 
 data = requests.get('https://gist.githubusercontent.com/lukasheinrich/75b80a2f8bc49e365bfb96e767c8a726/raw/a0946bc7590c76fec2b70de2f6f46208c0545c8d/BkgOnly.json').json()
+
+prepare_task = fxc.run(data, endpoint_id=pyhf_endpoint, function_id=prepare_func)
+
+w = None
+
+while not w:
+    try:
+        w = fxc.get_result(prepare_task)
+    except Exception as e:
+        print("prepare ", e)
+        sleep(30)
+
+print("--------------------")
+print(w)
+
+
 doc = requests.get('https://gist.githubusercontent.com/lukasheinrich/0bae2f9d6c2667d35cdff31d61092b16/raw/cb45fb6e7f7cdf7432834dd475f1353107602332/patch.json').json()
 
-res = fxc.run(data, doc, endpoint_id=pyhf_endpoint, function_id=f)
+res = fxc.run(w, doc, endpoint_id=pyhf_endpoint, function_id=f)
 
 result = None
 
